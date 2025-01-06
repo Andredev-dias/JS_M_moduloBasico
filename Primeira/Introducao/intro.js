ee4df84079c705ecf5221c2af3f340393217f2f4
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-01-06 11:57:48 -0300

    Add files via upload

---
+++ Primeira/Introducao/intro.js
@@ -0,0 +1,38 @@
+console.log("ola")
+console.log("André", "André", "André", "André")
+
+// VARIAVEIS
+// responsaveis por guardar dados na memória do computador, evitam repetição
+
+var nome = "André"
+nome = "Lucas"
+let sobrenome = "Silva"
+const verdadeiro = false
+const idade = 18
+var lanchonete = "Mc´Donald"
+
+// tipos dos dados
+// string, number, boolean
+
+console.log(nome, nome, nome, nome)
+console.log(2)
+console.log(lanchonete, typeof lanchonete)
+console.log(idade, typeof idade)
+console.log(verdadeiro, typeof verdadeiro)
+
+//concatenação
+console.log(nome + " " + sobrenome)
+
+//conta com variavel
+var preco = 25
+var quantidade = 3
+var total = preco * quantidade
+
+console.log(total, "TOTAL DA CONTA COM VARIAVEL")
+
+// virgula para separar variaveis declaradas ao mesmo tempo
+var loja = "Tunel do Rock", cidade, numero;
+cidade = "Curitiba"
+numero = 339
+console.log(loja, cidade, numero)
+
