772c57417b39a39705824193850210155ed67399
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-01-07 09:48:21 -0300

    Add files via upload

--- Primeira/Exercicios/exer.js
+++ Primeira/Exercicios/exer.js
@@ -1,35 +1,48 @@
-# JS_M_moduloBasico
-Aulas online sobre javascript
-
-Comandos git
-- git clone <caminho https do repositorio>
-
-
-Outros comandos do terminal
-- code . (abre o vsc na pasta referente)
-- cd <nome da pasta> (entrar nas pastas referentes)
-- ls (lista os arquivos)
-- cd .. (volta uma pasta)
-
-
-alteração no código
-
 // EXERCICIOS de variaveis
 
 //Declara uma variavel com a sua idade
 
+let idade = 36
+var idade2 = 36
+const idade3 = 36
+
 //Declarar uma variavel com a sua comida preferida e atribuir um valor
+
+const comidaPreferida = "Lasanha"
 
 // Declarar uma variavel e nao dar valor pra ela, sendo undefined
 
+let semValor
+
 // Declarar 5 variaveis diferentes sem valores
+
+var a,b,c,d,e
 
 // Declarar uma variavel contendo uma string
 
+var comida = "Batata"
+
 // Declare uma varivel contendo um numero dentro de uma string
+
+var numeroString = "345"
 
 // Declare uma variavel com seu nome, e outra com sobrenome, e concatene os dois, depois faça um template string dos 2
 
+var nome = "André"
+var sobrenome = "Dias"
+//concatenacao
+console.log( nome + " " +sobrenome)
+//template string
+console.log(`${nome} ${sobrenome}`)
+
 // Coloque a seguinte frase em uma variavel: It´s time
 
+var msg = "It´s time"
+
 // verifique o tipo de todas as variaveis criadas
+
+console.log(typeof idade, typeof nome, typeof msg, typeof a)
+
+// console.log(typeof numeroString)
+
+console.log(comidaPreferida)
---
+++ Primeira/RevisaoIntroducao/script.js
@@ -0,0 +1,51 @@
+// variaveis
+// var n = "batata"
+
+// console.log(n,n, n, n, n,n, n)
+
+// var nome = "André"
+// var nome = "batata"
+
+// let nome = "andre"
+
+// const nome = "André"
+
+var pessoa1 = ["1", "2", "3", nome]
+var pessoa2 = "gabriel"
+
+// console.log(pessoa1[3])
+
+var media 
+var n1 = 10
+var n2 = 7
+var n3 = 5
+
+media = (n1 + n2 + n3) / 3
+
+// console.log(media.toFixed(1))
+
+// console.log(n1, n2, n3, "oooo")
+
+var idade = 36
+
+// console.log(nome + " " +"tem" + " " + idade + " " +"anos")
+
+var nome = "josias",
+ sobrenome = "manul",
+ idade = 22,
+ altura = 1.87
+
+// console.log(nome, sobrenome)
+
+// console.log(naoExisto)
+
+var carro = "3sahuduisa96879067896;/.,"
+var Carro = "Mercedes"
+
+console.log(typeof carro,typeof  idade, typeof altura)
+
+console.log( carro,  idade,  altura)
+
+
+
+
