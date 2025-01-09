92218323149e6c2a7634d85a1c27dba392f733a5
Andre Dias <andredias.curitiba@gmail.com>
2025-01-09 11:49:34 -0300

    int usuario e pouco de html e css

---
+++ Terceira/IntUsuarioRef/script.js
@@ -0,0 +1,44 @@
+function calcular(id1, id2, respId, operador) {
+    // alert(id1 + " - " + id2 + " - " + respId + " - " + operador)
+    // alert(`${id1}, ${id2}, ${respId}, ${operador} - parametros da funcao`)
+    var num1 = parseFloat(document.getElementById(id1).value);
+    var num2 = parseFloat(document.getElementById(id2).value);
+    var resultado;
+
+    switch (operador) {
+      case "+":
+        resultado = num1 + num2;
+        break;
+      case "-":
+        resultado = num1 - num2;
+        break;
+      case "*":
+        resultado = num1 * num2;
+        break;
+      case "/":
+        if (num2 !== 0) {
+          resultado = num1 / num2;
+        } else {
+          resultado = "Erro: divisao por 0";
+        }
+        break;
+      default:
+        resultado = "Operador invalido";
+        break;
+    }
+    // if (Number.isInteger(resultado)) {
+    //   return (document.getElementById(respId).textContent = resultado);
+    // } else {
+    //   return (document.getElementById(respId).textContent =
+    //     resultado.toFixed(2));
+    // }
+    document.getElementById(respId).textContent = formatNumber(resultado);
+  }
+
+    function formatNumber(num) {
+      if (Number.isInteger(num)) {
+        return num;
+      } else {
+        return num.toFixed(2);
+      }
+    }
